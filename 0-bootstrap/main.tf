7d3df12888b7e605ac2b532bc02a6c0ea01acab8
Ilya Kisleyko <osterik@gmail.com>
2026-08-12 12:58:36 +0000

    add `backend_region`, update DDB table name

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -1,15 +1,20 @@
-variable "aws_region" {
-  description = "The region where the resources are located"
+variable "region" {
+  description = "The AWS region where EC2, VPC, and EKS resources are deployed"
   type        = string
 }
 
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket"
+variable "backend_region" {
+  description = "The AWS region containing the S3 backend bucket and CMDB DynamoDB table"
   type        = string
 }
 
-variable "dynamodb_table_name" {
-  description = "The name of the DynamoDB table"
+variable "backend_bucket" {
+  description = "The name of the S3 bucket used for Terraform state and S3 lock files"
+  type        = string
+}
+
+variable "cmdb_dynamodb_table" {
+  description = "The name of the DynamoDB table used by the CMDB"
   type        = string
 }
 
@@ -29,7 +34,7 @@ data "aws_iam_policy_document" "admin_restricted_policy" {
     condition {
       test     = "StringEquals"
       variable = "aws:RequestedRegion"
-      values   = [var.aws_region]
+      values   = [var.region]
     }
   }
 
@@ -49,8 +54,8 @@ data "aws_iam_policy_document" "admin_restricted_policy" {
     effect  = "Allow"
     actions = ["s3:*"]
     resources = [
-      "arn:aws:s3:::${var.s3_bucket_name}",
-      "arn:aws:s3:::${var.s3_bucket_name}/*"
+      "arn:aws:s3:::${var.backend_bucket}",
+      "arn:aws:s3:::${var.backend_bucket}/*"
     ]
   }
 
@@ -60,8 +65,8 @@ data "aws_iam_policy_document" "admin_restricted_policy" {
     effect  = "Allow"
     actions = ["dynamodb:*"]
     resources = [
-      "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.dynamodb_table_name}",
-      "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.dynamodb_table_name}/index/*"
+      "arn:aws:dynamodb:${var.backend_region}:${data.aws_caller_identity.current.account_id}:table/${var.cmdb_dynamodb_table}",
+      "arn:aws:dynamodb:${var.backend_region}:${data.aws_caller_identity.current.account_id}:table/${var.cmdb_dynamodb_table}/index/*"
     ]
   }
 }
@@ -69,7 +74,8 @@ data "aws_iam_policy_document" "admin_restricted_policy" {
 resource "aws_iam_policy" "custom_admin_policy" {
   name        = "CKAMockRegionalAdminPolicy"
   path        = "/"
-  description = "Admin access for VPC, EC2, IAM, EKS in ${var.aws_region} and specific S3/DDB resources"
+  description = "Admin access for VPC, EC2, IAM, EKS in ${var.region} and specific S3/DDB resources"
+  # description = "Admin access for VPC, EC2, and EKS in ${var.region}, IAM globally, and backend resources in ${var.backend_region}"  
   policy      = data.aws_iam_policy_document.admin_restricted_policy.json
 }
 
