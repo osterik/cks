101c7c412b7ecf61379669e7274511bf680d8c4a
Ilya Kisleyko <osterik@gmail.com>
2026-08-14 10:39:15 +0000

    fix for `KUBELET_EXTRA_ARGS` location
    see https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/kubelet-integration/ :
    
    > The file that can contain user-specified flag overrides with KUBELET_EXTRA_ARGS is sourced from /etc/default/kubelet (for DEBs), or /etc/sysconfig/kubelet (for RPMs).

--- terraform/modules/k8s_self_managment_v2/template/worker.sh
+++ terraform/modules/k8s_self_managment_v2/template/worker.sh
@@ -81,7 +81,7 @@ cat > $kubelet_config_url/20-labels-taints.conf  <<EOF
 [Service]
 Environment="KUBELET_EXTRA_ARGS=--node-labels=node_name=${node_name},${node_labels}"
 EOF
-echo "Environment=\"KUBELET_EXTRA_ARGS=--node-labels=node_name=${node_name},${node_labels}\"">/etc/sysconfig/kubelet
+echo "Environment=\"KUBELET_EXTRA_ARGS=--node-labels=node_name=${node_name},${node_labels}\"">/etc/default/kubelet
 systemctl enable kubelet
 systemctl restart kubelet
 
