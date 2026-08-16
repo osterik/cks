333d03d1d4d6c0be2dbc15ed556851a531c554c6
Ilya Kisleyko <osterik@gmail.com>
2026-08-16 18:06:30 +0000

    fix comment formatting

--- terraform/modules/work_pc_v2/var.tf
+++ terraform/modules/work_pc_v2/var.tf
@@ -67,8 +67,7 @@ variable "work_pc" {
       private_key = string
       pub_key     = string
     })
-    test_url          = string # "https:..." dowload tests from given URL
-                               # "file:..." inject test from the local filesystem
+    test_url          = string # "https:..." dowload tests from given URL, "file:..." inject test from the local filesystem
     exam_time_minutes = string
     util = object({
       kubectl_version = string
